d2dabe49df5c88ba7207e514571d84aab0923f4d
roberto <rvneto.rs@gmail.com>
2024-05-14 20:24:55 -0300

    incluido mapeamento

--- src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
+++ src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -3,6 +3,7 @@ package com.client.ws.rasmooplus.service.impl;
 import com.client.ws.rasmooplus.dto.SubscriptionTypeDto;
 import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoundException;
+import com.client.ws.rasmooplus.mapper.SubscriptionTypeMapper;
 import com.client.ws.rasmooplus.model.SubscriptionType;
 import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
@@ -34,25 +35,14 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
         if (nonNull(dto.getId())) {
             throw new BadRequestException("Id deve ser nulo");
         }
-
-        return repository.save(SubscriptionType.builder()
-                .name(dto.getName())
-                .accessMonth(dto.getAccessMonth())
-                .price(dto.getPrice())
-                .productKey(dto.getProductKey())
-                .build());
+        return repository.save(SubscriptionTypeMapper.fromDtoToEntity(dto));
     }
 
     @Override
     public SubscriptionType update(Long id, SubscriptionTypeDto dto) {
         getSubscriptionType(id);
-        return repository.save(SubscriptionType.builder()
-                .id(id)
-                .name(dto.getName())
-                .accessMonth(dto.getAccessMonth())
-                .price(dto.getPrice())
-                .productKey(dto.getProductKey())
-                .build());
+        dto.setId(id);
+        return repository.save(SubscriptionTypeMapper.fromDtoToEntity(dto));
     }
 
     @Override
