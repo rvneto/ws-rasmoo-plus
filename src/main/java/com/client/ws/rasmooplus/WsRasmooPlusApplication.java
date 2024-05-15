c554651a6911b44d614bbb1c3e55fcc9c4918e94
roberto <rvneto.rs@gmail.com>
2024-05-14 21:59:52 -0300

    documentando com swagger (não funcionando)

--- src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
+++ src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
@@ -2,12 +2,14 @@ package com.client.ws.rasmooplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class WsRasmooPlusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WsRasmooPlusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WsRasmooPlusApplication.class, args);
+    }
 
 }
