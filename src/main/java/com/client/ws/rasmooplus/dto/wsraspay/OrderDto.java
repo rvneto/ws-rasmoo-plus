0a00f22781623594490d4565eff45b7d24de5ea2
roberto <rvneto.rs@gmail.com>
2024-05-20 20:41:53 -0300

    criando dtos de intregraçao

---
+++ src/main/java/com/client/ws/rasmooplus/dto/wsraspay/OrderDto.java
@@ -0,0 +1,17 @@
+package com.client.ws.rasmooplus.dto.wsraspay;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDto {
+
+    private String id;
+    private String customerId;
+    private Long discount;
+    private String productAcronym;
+
+}
---
+++ src/main/java/com/client/ws/rasmooplus/dto/wsraspay/PaymentDto.java
@@ -0,0 +1,16 @@
+package com.client.ws.rasmooplus.dto.wsraspay;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDto {
+
+    private CreditCardDto creditCard;
+    private String customerId;
+    private String orderId;
+
+}
